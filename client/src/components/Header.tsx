efc4d4d248643d58a9fcca3328bdc93a0649dd03
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-18 21:01:26 +0200

    add Links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between px-8 py-4 gap-36">
+      <Logo />
+      <nav className="flex justify-between gap-24">
+        <Link
+          to={"/todo"}
+          className="text-2xl font-bold text-gray-400 hover:text-gray-300 duration-300 hover:drop-shadow-[0_0_1em_#646cffaa] hover:underline"
+        >
+          Todo
+        </Link>
+        <Link
+          to={"/notes"}
+          className="text-2xl font-bold text-gray-400 hover:text-gray-300 duration-300 hover:drop-shadow-[0_0_1em_#646cffaa] hover:underline"
+        >
+          Notes
+        </Link>
+      </nav>
+      <div className="text-2xl font-bold text-gray-500">User😀</div>
+    </div>
+  );
 };
 
 export default Header;
