7c3dc6fefc89e2c582dccc214747ff453cabf546
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-19 20:10:33 +0200

    add styles to Link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,24 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const location = useLocation();
+
   return (
     <div className="flex justify-between px-8 py-4 gap-36">
       <Logo />
       <nav className="flex justify-between gap-24">
         <Link
-          to={"/todo"}
-          className="text-2xl font-bold text-gray-400 hover:text-gray-300 duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
-          after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+          to={"/todos"}
+          className={`text-2xl font-bold font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
+            ${
+              location.pathname === "/todos"
+                ? "text-gray-300 drop-shadow-[0_0_0_#074da8aa] after:scale-x-100"
+                : "text-gray-400 hover:text-gray-300 hover:after:scale-x-100"
+            }
+          after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100`}
         >
           Todo
         </Link>
         <Link
           to={"/notes"}
-          className="text-2xl font-bold text-gray-400 hover:text-gray-300 duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
-          after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+          className={`text-2xl font-bold font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
+          ${
+            location.pathname === "/notes"
+              ? "text-gray-300 drop-shadow-[0_0_0_#074da8aa] after:scale-x-100"
+              : "text-gray-400 hover:text-gray-300 hover:after:scale-x-100"
+          }
+          after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100`}
         >
           Notes
         </Link>
