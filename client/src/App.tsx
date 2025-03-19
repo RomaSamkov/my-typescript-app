6fa222871901fbc4f836b14324f1daa2054ece03
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-19 20:10:19 +0200

    add Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import Todo from "./components/Todo";
+import Notes from "./components/Notes";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/todos" element={<Todo />} />
+        <Route path="/notes" element={<Notes />} />
       </Routes>
     </>
   );
