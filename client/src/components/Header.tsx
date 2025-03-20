96488e22122b7d31d0259247515d4128c3038e03
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-20 13:46:13 +0200

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,41 +1,45 @@
 import { Link, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 
-type Props = {};
+const active =
+  "after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100";
 
-const Header = (props: Props) => {
+const Header = () => {
   const location = useLocation();
 
   return (
-    <div className="flex justify-between px-8 py-4 gap-36">
+    <div className="flex justify-between px-8 py-4 gap-24 max-[720px]:gap-4 max-[720px]:text-xs">
       <Logo />
-      <nav className="flex justify-between gap-24">
+      <nav className="flex justify-between gap-24 max-[720px]:gap-2 max-[720px]:hidden">
         <Link
           to={"/todos"}
-          className={`text-2xl font-bold font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
+          className={`relative text-2xl font-bold font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
             ${
               location.pathname === "/todos"
                 ? "text-gray-300 drop-shadow-[0_0_0_#074da8aa] after:scale-x-100"
                 : "text-gray-400 hover:text-gray-300 hover:after:scale-x-100"
             }
-          after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100`}
+          ${active}`}
         >
           Todo
         </Link>
         <Link
           to={"/notes"}
-          className={`text-2xl font-bold font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
+          className={`relative text-2xl font-bold font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa]
           ${
             location.pathname === "/notes"
               ? "text-gray-300 drop-shadow-[0_0_0_#074da8aa] after:scale-x-100"
               : "text-gray-400 hover:text-gray-300 hover:after:scale-x-100"
           }
-          after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100`}
+          ${active}`}
         >
           Notes
         </Link>
       </nav>
-      <div className="text-2xl font-bold text-gray-500">User😀</div>
+      <div className="flex text-2xl font-bold text-gray-500">
+        <span>😀</span>
+        <p>User</p>
+      </div>
     </div>
   );
 };
