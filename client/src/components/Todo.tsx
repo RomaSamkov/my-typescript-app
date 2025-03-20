8ae45327f0bc8257cc2c89cd4f0559505916e93e
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-20 13:46:26 +0200

    Update Todo.tsx

--- client/src/components/Todo.tsx
+++ client/src/components/Todo.tsx
@@ -2,20 +2,23 @@ type Props = {};
 
 const Todo = (props: Props) => {
   return (
-    <div className="flex items-center px-8">
-      <form className="">
-        <label htmlFor="todo">Todo</label>
-        <input
-          type="text"
-          id="todo"
-          name="todo"
-          placeholder="What to do ..."
-          className="border rounded-2xl p-2"
-        />
-      </form>
-      <button className="border rounded-2xl bg-gray-700 p-2 cursor-pointer hover:bg-gray-500 hover:text-gray-800 hover:border-white">
-        Add To Do
-      </button>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="p-4">Add a task to your TODO list:</h2>
+      <div className="flex items-center px-8">
+        <form className="">
+          <label htmlFor="todo"></label>
+          <input
+            type="text"
+            id="todo"
+            name="todo"
+            placeholder="your task ..."
+            className="border rounded-2xl px-4 py-2"
+          />
+        </form>
+        <button className="border rounded-2xl bg-gray-700 p-2 cursor-pointer hover:bg-gray-300 hover:text-gray-800 hover:border-white">
+          Add task
+        </button>
+      </div>
     </div>
   );
 };
