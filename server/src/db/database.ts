9b8b1ea273e899f0dd943f8e884ec556265173ee
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-31 14:46:21 +0300

    Create database.ts

---
+++ server/src/db/database.ts
@@ -0,0 +1,17 @@
+export const database = [
+  {
+    id: 1,
+    title: "Todo 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Listen to music",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Watch film",
+    completed: false,
+  },
+];
