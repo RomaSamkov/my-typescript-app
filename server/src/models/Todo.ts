d282b7e63893a8badd8ccc0be66866e1061b6d86
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-02 14:02:08 +0300

    Update Todo.ts

--- server/src/models/Todo.ts
+++ server/src/models/Todo.ts
@@ -1,1 +1,20 @@
-const Todo = {};
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;
