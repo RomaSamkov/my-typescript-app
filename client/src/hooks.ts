54565bba4f93624f48ca05428f1d2b2e3af1eb91
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-03 19:16:32 +0300

    Update hooks.ts

--- client/src/hooks.ts
+++ client/src/hooks.ts
@@ -1,5 +1,14 @@
-import { useDispatch, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
+// Правильний кастомний хук для dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+// Правильний кастомний хук для selector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// import { useDispatch, useSelector } from "react-redux";
+// import type { RootState, AppDispatch } from "./store";
+
+// export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+// export const useAppSelector = useSelector.withTypes<RootState>();
