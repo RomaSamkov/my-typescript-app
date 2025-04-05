af4d822bd01e378832f3de48a72509048b4be545
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-05 12:46:02 +0300

    add deleteTodo

--- server/src/routes/todosRoutes.ts
+++ server/src/routes/todosRoutes.ts
@@ -1,9 +1,14 @@
-import { addTodo, getAllTodos } from "../controllers/todosController";
+import {
+  addTodo,
+  deleteTodo,
+  getAllTodos,
+} from "../controllers/todosController";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", getAllTodos);
 router.post("/add", addTodo);
+router.delete("/:id", deleteTodo);
 
 export default router;
