a3ae6839cd423e1a6e71b6ff0ed1a33dfce47d61
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-09 16:33:35 +0300

    add new title

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,35 +1,46 @@
-import { Save } from "lucide-react";
+import { useState } from "react";
 
 type Props = {
   closeModal: () => void;
+  onSave: (updatedTitle: string) => void;
+  currentTitle: string;
 };
 
-const Modal = ({ closeModal }: Props) => {
+const Modal = ({ closeModal, onSave, currentTitle }: Props) => {
+  const [newTitle, setNewTitle] = useState(currentTitle);
+
+  const handleSave = () => {
+    if (newTitle.trim()) {
+      onSave(newTitle);
+    }
+  };
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800/85 z-50">
-      <div className="bg-gray-600 p-6 rounded shadow-lg min-w-[300px]">
-        <h3 className="text-lg font-bold mb-4">Редагувати тудушку</h3>
-        <div className="flex">
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-900/85 z-50">
+      <div className="flex  flex-col items-center bg-gray-700 p-6 rounded-2xl shadow-lg min-w-[300px]">
+        <h3 className="text-lg font-bold mb-4">Edit Todo</h3>
+        <div className="flex p-4">
           <form>
             <input
               type="text"
-              placeholder="edit todo ..."
-              className="border rounded-2xl"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="edit task ..."
+              className="border rounded-2xl p-2 outline-white"
             />
           </form>
-          <button>
-            <Save />
-          </button>
         </div>
         <div className="flex justify-end gap-2">
           <button
-            onClick={closeModal}
-            className="bg-gray-300 px-4 py-1 rounded"
+            onClick={handleSave}
+            className="bg-blue-600 text-white px-6 py-1 rounded cursor-pointer hover:bg-blue-800"
           >
-            Скасувати
+            Save
           </button>
-          <button className="bg-blue-500 text-white px-4 py-1 rounded">
-            Зберегти
+          <button
+            onClick={closeModal}
+            className="bg-gray-500 px-4 py-1 rounded cursor-pointer hover:bg-gray-800"
+          >
+            Cancel
           </button>
         </div>
       </div>
