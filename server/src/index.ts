a9ec8e17cce2b7c76176c980033ab39b12917568
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-10 14:16:44 +0300

    add notesRoutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/connectDB";
 import todosRoutes from "./routes/todosRoutes";
+import notesRoutes from "./routes/notesRoutes";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use("/api/notebook/todos", todosRoutes);
+app.use("/api/notebook/notes", notesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
