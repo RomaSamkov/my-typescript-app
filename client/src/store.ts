8ee6e21c27aa1e9734ceb5206ec99c4a906a48a7
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-10 15:24:12 +0300

    add notesReducer

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./slices/TodosSlice";
+import notesReducer from "./slices/NotesSlice";
 
 export const store = configureStore({
-  reducer: { todos: todosReducer },
+  reducer: { todos: todosReducer, notes: notesReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
