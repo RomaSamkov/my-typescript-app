364a51105e320894c61cbc57ecc0534318b8537a
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-11 19:12:36 +0300

    Update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,19 +1,43 @@
-type Props = {};
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store"; // Adjust the path to your store file
+import { useEffect } from "react";
+import { authCheck, logout } from "../slices/authSlice";
 
-const HomePage = (props: Props) => {
+const HomePage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+
+  useEffect(() => {
+    dispatch(authCheck());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logout());
+      alert("Logout successful!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="flex justify-center">
-      <div className="flex gap-1 text-2xl font-bold text-gray-500 max-[720px]:hidden">
-        <div className="font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa] bg-gray-600 border rounded-2xl px-2 text-gray-400 hover:text-gray-300 hover:after:scale-x-100 cursor-pointer">
-          Login
-        </div>
-
-        <div className="font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa] bg-gray-600 border rounded-2xl px-2 text-gray-400 hover:text-gray-300 hover:after:scale-x-100 cursor-pointer">
-          Register
-        </div>
+    <p>
+      <div>
+        {user ? <p>Welcome, {user.username}!</p> : <p>Please log in.</p>}
       </div>
-    </div>
+      <button onClick={handleLogout}>Logout</button>
+    </p>
   );
+  // const { user } = useSelector((state: RootState) => state.auth);
+  // return (
+  //   <div className="flex justify-center gap-12">
+  //     {!user ? "No user" : `Hello ${user?.username}`}
+  //   </div>
+  // );
 };
 
 export default HomePage;
