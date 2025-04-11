c2ef3941e22111ecd88aa4acb55c6f742fc24e28
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-11 19:12:21 +0300

    Create PrivateRoute.tsx

---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+
+type PrivateRouteProps = {
+  children: React.ReactNode;
+};
+
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  console.log("user:", user);
+
+  // Якщо користувач не залогінений, перенаправляємо на сторінку логіну
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  // Якщо користувач залогінений, рендеримо дочірній компонент
+  return children;
+};
+
+export default PrivateRoute;
