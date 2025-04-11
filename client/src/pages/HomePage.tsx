ba358ea7cd53384be1d566710fe3e80f128e96b2
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-11 12:00:40 +0300

    Update HomePage.tsx

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
 type Props = {};
 
 const HomePage = (props: Props) => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="flex gap-1 text-2xl font-bold text-gray-500 max-[720px]:hidden">
+        <div className="font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa] bg-gray-600 border rounded-2xl px-2 text-gray-400 hover:text-gray-300 hover:after:scale-x-100 cursor-pointer">
+          Login
+        </div>
+
+        <div className="font-serif duration-300 hover:drop-shadow-[0_0_1em_#646cffaa] bg-gray-600 border rounded-2xl px-2 text-gray-400 hover:text-gray-300 hover:after:scale-x-100 cursor-pointer">
+          Register
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
