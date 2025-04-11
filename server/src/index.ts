9c564167937a91bf4c6ab53cc6bfa96a83bd8b47
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-11 16:42:13 +0300

    add authRoutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./config/connectDB";
 import todosRoutes from "./routes/todosRoutes";
 import notesRoutes from "./routes/notesRoutes";
+import authRoutes from "./routes/authRoutes";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,9 +13,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use("/api/notebook/todos", todosRoutes);
 app.use("/api/notebook/notes", notesRoutes);
+app.use("/api/notebook/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
