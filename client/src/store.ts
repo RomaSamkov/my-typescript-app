13dae2ec9882320fa0f1465e3de0f32d2eb0544e
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-11 19:17:56 +0300

    add authReducer

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./slices/TodosSlice";
 import notesReducer from "./slices/NotesSlice";
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
-  reducer: { todos: todosReducer, notes: notesReducer },
+  reducer: { todos: todosReducer, notes: notesReducer, auth: authReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
