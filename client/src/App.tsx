0a11a43bd83fd35085cbdcbdd69e3e2e94a9146d
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-11 19:12:05 +0300

    add PrivateRoute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Todo from "./components/Todo";
 import Notes from "./components/Notes";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Logout from "./components/Logout";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -10,8 +14,18 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/todos" element={<Todo />} />
+        <Route
+          path="/todos"
+          element={
+            <PrivateRoute>
+              <Todo />
+            </PrivateRoute>
+          }
+        />
         <Route path="/notes" element={<Notes />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </>
   );
