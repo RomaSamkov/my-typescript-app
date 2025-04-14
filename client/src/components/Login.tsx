7215b616d536f63a656ba337687e54cc679a223a
RomaSamkov <Romansamkov.rs@gmail.com>
2025-04-14 12:22:24 +0300

    add styles

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../slices/authSlice";
 import { AppDispatch, RootState } from "../store";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -15,36 +15,45 @@ const Login = () => {
     try {
       await dispatch(login({ email, password }));
       alert("Login successful!");
-      navigate("/todos");
+      navigate("/");
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <div className="flex flex-col gap-2 items-center justify-center">
-      <h2>Login</h2>
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <h2 className="font-serif font-bold text-2xl py-8">Login</h2>
       <input
         type="email"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="border"
+        className="border rounded-2xl p-2 outline-amber-100 w-[300px]"
       />
       <input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className="border"
+        className="border rounded-2xl p-2 outline-amber-100 w-[300px]"
       />
       <button
         onClick={handleLogin}
         disabled={loading}
-        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800"
+        className="bg-blue-600 text-white px-4 py-2 rounded-2xl hover:bg-blue-800 w-[300px] cursor-pointer"
       >
         Login
       </button>
+      <p>
+        If you haven't an account please{" "}
+        <Link
+          to="/register"
+          className="underline text-blue-600 hover:text-blue-800"
+        >
+          Register
+        </Link>
+      </p>
       {error && <p>{error}</p>}
     </div>
   );
